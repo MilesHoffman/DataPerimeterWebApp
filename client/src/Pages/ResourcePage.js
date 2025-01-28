3d3a0230dcf4f17d7f3f348a617da16adda336db
Brayden <braydenboyer@yahoo.com>
2025-01-27 20:35:16 -0500

    Progress on Resource page

--- client/src/Pages/ResourcePage.js
+++ client/src/Pages/ResourcePage.js
@@ -12,63 +12,127 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
+
+import testImage1 from '../tempResources/testImage1.jpg'
+import testImage2 from '../tempResources/testImage2.jpg'
+import testImage3 from '../tempResources/testImage3.jpg'
+import testFile1 from '../tempResources/testFile.txt'
 const resources = [
+    {
+        name: 'Document  1',
+        type: 'document',
+        src: testFile1,
+    },
     {
         name: 'Image 1',
         type: 'image',
-        src: '../tempResources/testFile.txt',
-    },
-    {
-        name: 'Document 1',
-        type: 'document',
-        src: '../tempResources/testImage1.jpg',
+        src: testImage1,
     },
     {
         name: 'Image 2',
         type: 'image',
-        src: '../tempResources/testImage2.jpg',
+        src: testImage2,
     },
-
+    {
+        name: 'Image 3',
+        type: 'image',
+        src: testImage3,
+    },
 ];
 function ResourcePage() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
 
         <div>
             <h1>Resource Page</h1>
 
             <Grid container spacing={2}>
-
                 {resources.map((resource) => (
                     <Grid item xs={12} sm={6} md={4} key={resource.name}>
-
-                        <Card sx={{height: 300}}>
-
-                            <CardActionArea>
-
-                                {resource.type === "image" && (
+                        <Card sx={{height: 350, display: "flex", flexDirection: "column"}}>
+                            {/* Add flex display and column direction */}
+                            <CardActionArea sx={{flex: 1, display: "flex"}}>
+                                {/* Allow CardActionArea to take available space */}
+                                {resource.type === "image" ? (
                                     <CardMedia
                                         component="img"
-
                                         height="200"
                                         image={resource.src}
                                         alt={resource.name}
+                                        sx={{objectFit: "contain"}}
                                     />
+                                ) : (
+                                    <CardContent
+                                        sx={{
+                                            textAlign: "center",
+                                            flex: 1,
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            alignItems: "center",
+                                        }}
+                                    >
+                                        {/* Center the icon and allow it to take space */}
+                                        <InsertDriveFileIcon sx={{fontSize: 80}}/>
+                                    </CardContent>
                                 )}
-                                <CardContent>
+                            </CardActionArea>
+                            <CardContent
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <div>
                                     <Typography gutterBottom variant="h5" component="div">
                                         {resource.name}
                                     </Typography>
                                     <Typography variant="body2" color="text.secondary">
                                         {resource.type}
                                     </Typography>
-                                </CardContent>
-                            </CardActionArea>
+                                </div>
+                                <IconButton
+                                    aria-label="more"
+                                    id="long-button"
+                                    aria-controls={open ? "long-menu" : undefined}
+                                    aria-expanded={open ? "true" : undefined}
+                                    aria-haspopup="true"
+                                    onClick={handleClick}
+                                >
+                                    <MoreVertIcon/>
+                                </IconButton>
+                                <Menu
+                                    id="long-menu"
+                                    MenuListProps={{
+                                        "aria-labelledby": "long-button",
+                                    }}
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem onClick={handleClose}>Option 1</MenuItem>
+                                    <MenuItem onClick={handleClose}>Option 2</MenuItem>
+                                    <MenuItem onClick={handleClose}>Option 3</MenuItem>
+                                </Menu>
+                            </CardContent>
                         </Card>
                     </Grid>
                 ))}
             </Grid>
         </div>
+
         /*
         <div>
             <h1>Resource Page</h1>
