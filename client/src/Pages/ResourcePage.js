268df6a557767a77e01c746794fdcad0d9da134d
Brayden <braydenboyer@yahoo.com>
2025-02-05 12:22:51 -0500

    Progress on diplaying Resources to the page from aws account

--- client/src/Pages/ResourcePage.js
+++ client/src/Pages/ResourcePage.js
@@ -1,102 +1,104 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Stack from '@mui/material/Stack';
-
-
-
-import testImage1 from '../tempResources/testImage1.jpg'
-import testImage2 from '../tempResources/testImage2.jpg'
-import testImage3 from '../tempResources/testImage3.jpg'
-import testFile1 from '../tempResources/testFile.txt'
+import { useEffect, useState } from "react";
 import axios from "axios";
-const resources = [
-    {
-        name: 'Document  1',
-        type: 'document',
-        src: testFile1,
-    },
-    {
-        name: 'Image 1',
-        type: 'image',
-        src: testImage1,
-    },
-    {
-        name: 'Image 2',
-        type: 'image',
-        src: testImage2,
-    },
-    {
-        name: 'Image 3',
-        type: 'image',
-        src: testImage3,
-    },
-];
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+import IconButton from "@mui/material/IconButton";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { Stack } from "@mui/material";
+
 function ResourcePage() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [resources, setResources] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     const open = Boolean(anchorEl);
+
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
+
     const handleClose = () => {
         setAnchorEl(null);
     };
 
-    const handleTest = async () => {
-        const response = await axios.post("http://localhost:5000/api/resource", {
-            accessKeyId: 'ASIAVIOZFWO2ICG57MKK',
-            secretAccessKey: 'q0PwxSHXvjRi8/DPhbeLVq4tvn3gvZyTbfEMYTIn',
-            sessionToken: 'IQoJb3JpZ2luX2VjEA0aCXVzLWVhc3QtMiJHMEUCIDbgGYoLHKgSeS2iHlk7DbbMaFRXnovX3iwbtI5UEwaXAiEA4ffU7s1m5QA3KJqYkW7EnTflKRORRg8gv32L0uUGe0MqxAQIJhAAGgwzNjE3Njk1Nzk0NDQiDN//pL/0y+XLyLXEUyqhBJI6lC8iS7AOfS64zUZlJj2FIalh1ye7oDWR8qT7T2ygzBh0n//8zTkf3uIK7oXCBaHBgkVTmnZvsD5e056EnntNQ6GLWXX3WLyy/9M602Gk4tnxktCunj7Ag7TJ4CFLoP8QeUMrRCxC5ZIaeqxiKD06DLggJxqYDr+LRUP/+Q4DWJ9mHY24GMWjnlFbonBz/zG6zy0bzxV7pKNiJJYnYcPH999V/9/A0B3Rh+4Q5q7T+1zhmJK0I5jwQ+10wWQZTZv7Fka53P8i9oEXcactcJIZTNqF1cO0CZrP03c81AFCMG+iD0eQCpPjOV/fd+KlL/l4cBTOhjug44+zjxk86hD0G1cxYZOTds/n4d/ezIwQ98c8k3LahWrKGYFIOd4Lo1LgZd0CndOD5mXzG4TNwaKjXacftBjbDyI8G9aNogdAJQ055HbrOI8e6CbtQhZvpKP6XZ5Z3IUtDSe8DnfU0qpDPdjQJcQ7maPgam+3FmIhDUq4FNPkKNMa1FC8y2WyX/pmOEhP6GFv6yPbLK3Vq3XhoYw0lJPsU2XslSsQuC1pd7UA3i1SceG0Ju4u3W/gn5C6GJTNETgIvgqYzFgSVc7C6jScLrxukU3H+i0s/Ohy6SZIbNF+F+LCJtvJ+ZigIE9fACyDwrfGj2TKvmO+g05WukaR1UnMol+v23slc9z5SfhnYc6JHe58sdcqzE1s5tUKEckogt20+5SnuqKrdQ5qMM22hr0GOoUCh1yx0Yc6rHE7GLNrDEE6x7rGgTkEJaldtgtptZnNVhla/Ar9CfIaMwJQgDXLfVJ2R4dXF7DO2yPNiS1N2U3sBkYCnPKvwLJtf5N1pwJ6WV7GrYWrq5KxrPT4tns+osU/D+b6+Dyz5a4ZxixV0IwcNIOBAV73COznq4vOKcRBNTMHRFFnLhJFGyiEvUnh378fBw3mq2rbINmgT9Bwfbf19vVCAI7YRwBEQJDIPRMKC1zu9+GvbPworrBPMTk9FyGbCNtEHuEiZ29q9ggesk1eXqhezAFUQM4rmHkKzZk4DJlRQS6qRXrp1k5BM0qAAUOw86+ZDur7i29CQ+UL1zPmQuQUz7QS',
-            bucketName: "puppy-pics-s3"
+    const fetchResources = async () => {
+        setLoading(true);
+        setError(null);
 
-        });
+        try {
+            const response = await axios.post("http://localhost:5000/api/resource", {
+                accessKeyId: 'ASIAVIOZFWO2OYNJWQY4',
+                secretAccessKey:'b9lIbhD5sgjSuv8TOI4uBzjnuZVHCESN12V59uA6',
+                sessionToken:'IQoJb3JpZ2luX2VjEDEaCXVzLWVhc3QtMiJIMEYCIQDMGJMIxLg4azjjCTllw5X2l3FeQivfzgmLBwBf7/OZ6wIhAOt39jPo2x6xtn7k95Ms9jOliu4VyaqQRxr9S9HUz0hvKsQECEoQABoMMzYxNzY5NTc5NDQ0IgyqWilNzrpn+xxvOW0qoQSeMTUBXNpQMJ9wKoKxxlWjvfutzqVQPLsy/ETqFAFc2FmzRTKIkcNJqTJLZWOLO+6xgwK/Z0Zol6JOyerHIOdoZWFLq6rU3EpY+b90tf8tyIoByeRig2FISoj4hGLa2odt74ZPNnLMnD05ofsdjGuPfFMURhmrqnCLW48zCbUGVwaBod5+xTA3bYxFhy/ZH9sHuyr9Eznr/M7DalL9elwRfMh+S/KTrBBM5hqUNumLqPr5Sa1jKUwlAU6a8NuKhoO/XtGRg1cW0Ykx/HKygYQF/0stqoQmvg7MR+xw7VWNCQM362hnmSBzV9kn5moQoC6/pDkAxPumqiElYc9InuA0C1tQvHpWa9Vm5uUh2M5mVCb+EZMp+S3pP9agMHiubA9JmviV7KVQtv9zNe+nA3xxpbW//CbaAXoYzOnvb4Cd8NRnULGT+T3TEJcUoOavMzU4qxITK4N1jUSK19gP/eEZtmDQhABnKh6QQfDIP8ntqangkXJib1wKKTho8utyVKY5btSnoLpA+P2CspqSy/rw6dwph0E6dpFiXQ2fCVYYwZzbSWuf4WojRmGcXTLHqvhh8zd/7bjWC6gbEBvw99wXkv+v2Tv0V9E4Uw0ljmSt/sE1YHoOOuR5V4MDOnqrU36zjDpUlvXRvNx61JxwrbK4EYrGDbIMPmHebwcBBjEoZaTmziwHND5v2+GtLA7iN2CJaapSaEVYXPt1/TwwYQYyQTDeto69BjqEAgRAWmh/hMP2oQ7ZwTEHl+NUJbcSXjw708xaylBPmxur34K312eFtSclMzW6KQL8PydtgkoGBe1Q63iB+9/UFUFCufOVUPAk9ogy57SbpaA7uJs03BwBBSlAGCEbO9e83wUyxxto8PFeSvLkoUSWEBZXjmNntzwjnUUCD8i/rlNF9vLdMNxSz3/j+AKJu9krII9ZmTg+50Ps33M7xGpTd3ED4BNEFXe8r95PjfKUKCf4O2EUKHZckUDl6g7bjxDm5whfHBAhkk7zu7dsMMT5MmJduIz3XIfT4i4X9UNivZSJRG7prB74WFAwBT45mGMDUzM8UuLv757Dm0zYHc7+NfsN9s/h',
+                bucketName: "puppy-pics-s3",
+            });
 
-        console.log(response)
+            setResources(response.data || {});
+        } catch (err) {
+            setError(err);
+            console.error("Error fetching resources:", err);
+        } finally {
+            setLoading(false);
+        }
     };
 
+    useEffect(() => {
+        //fetchResources();
+        console.log('test')
+        console.log('test2')
+    },[]); // The empty dependency array ensures the effect runs only once
+
+    if (loading) {
+        return <div>Loading resources...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
     return (
-
         <div>
             <Stack direction="row" alignItems="center" spacing={1}>
                 <h1>Resource Page</h1>
-                <IconButton onClick={handleTest} variant="contained" aria-label="add">
+                <IconButton
+                    onClick={fetchResources}
+                    variant="contained"
+                    aria-label="add"
+                >
                     <AddIcon />
                 </IconButton>
             </Stack>
 
-
             <Grid container spacing={2}>
-                {resources.map((resource) => (
-                    <Grid item xs={12} sm={6} md={4} key={resource.name}>
-                        <Card sx={{height: 350, display: "flex", flexDirection: "column"}}>
-
-                            <CardActionArea sx={{flex: 1, display: "flex"}}>
-
-                                {resource.type === "image" ? (
+                {Object.values(resources).map((resource, index) => (
+                    <Grid item xs={12} sm={6} md={3} lg={3} key={index}>
+                        <Card
+                            sx={{ height: 350, display: "flex", flexDirection: "column" }}
+                        >
+                            <CardActionArea sx={{ flex: 1, display: "flex" }}>
+                                {resource.type === "image"? (
                                     <CardMedia
                                         component="img"
                                         height="200"
                                         image={resource.src}
                                         alt={resource.name}
-                                        sx={{objectFit: "contain"}}
+                                        sx={{ objectFit: "contain" }}
                                     />
-                                ) : (
+                                ): (
                                     <CardContent
                                         sx={{
                                             textAlign: "center",
@@ -106,8 +108,7 @@ function ResourcePage() {
                                             alignItems: "center",
                                         }}
                                     >
-
-                                        <InsertDriveFileIcon sx={{fontSize: 80}}/>
+                                        <InsertDriveFileIcon sx={{ fontSize: 80 }} />
                                     </CardContent>
                                 )}
                             </CardActionArea>
@@ -129,12 +130,12 @@ function ResourcePage() {
                                 <IconButton
                                     aria-label="more"
                                     id="long-button"
-                                    aria-controls={open ? "long-menu" : undefined}
-                                    aria-expanded={open ? "true" : undefined}
+                                    aria-controls={open? "long-menu": undefined}
+                                    aria-expanded={open? "true": undefined}
                                     aria-haspopup="true"
                                     onClick={handleClick}
                                 >
-                                    <MoreVertIcon/>
+                                    <MoreVertIcon />
                                 </IconButton>
                                 <Menu
                                     id="long-menu"
@@ -145,8 +146,20 @@ function ResourcePage() {
                                     open={open}
                                     onClose={handleClose}
                                 >
-                                    <MenuItem onClick={handleClose} variant="contained" startIcon={<EditIcon />}>Modify</MenuItem>
-                                    <MenuItem onClick={handleClose} variant="contained" startIcon={<DeleteIcon />}>Delete</MenuItem>
+                                    <MenuItem
+                                        onClick={handleClose}
+                                        variant="contained"
+                                        startIcon={<EditIcon />}
+                                    >
+                                        Modify
+                                    </MenuItem>
+                                    <MenuItem
+                                        onClick={handleClose}
+                                        variant="contained"
+                                        startIcon={<DeleteIcon />}
+                                    >
+                                        Delete
+                                    </MenuItem>
                                     <MenuItem onClick={handleClose}>Option 3</MenuItem>
                                 </Menu>
                             </CardContent>
@@ -155,28 +168,6 @@ function ResourcePage() {
                 ))}
             </Grid>
         </div>
-
-        /*
-        <div>
-            <h1>Resource Page</h1>
-            <p>This is the resource page content.</p>
-
-            <Link to="/">
-                <Button variant="contained">Back to Home</Button>
-            </Link>
-            <Button variant="contained" startIcon={<EditIcon />}>
-                Modify
-            </Button>
-
-            <Button variant="contained" startIcon={<DeleteIcon />}>
-                Delete
-            </Button>
-            <Button variant="contained" startIcon={<AddIcon />}>
-                Add
-            </Button>
-        </div>
-        */
-
     );
 }
 
